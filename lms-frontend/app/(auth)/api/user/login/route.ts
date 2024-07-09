acf5b3443d08ec17fedab11c13b63584f5ffd4b0
harsh1924 <harshrpanwar@gmail.com>
2024-07-09 15:15:10 +0530

    Post Host Changes

--- lms-frontend/app/(auth)/api/user/login/route.ts
+++ lms-frontend/app/(auth)/api/user/login/route.ts
@@ -40,7 +40,7 @@ export async function POST(request: NextRequest) {
             user
         })
 
-        response.cookies.set('token', token, { domain: 'skilltechindia.net', httpOnly: true });
+        response.cookies.set('token', token, { domain: 'skilltechindia.net', httpOnly: true, path: '/' });
 
         return response;
 

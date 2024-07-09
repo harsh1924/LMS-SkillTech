8364633cdbfe4110e2e2cc7d7b6b991bfe055dd1
harsh1924 <harshrpanwar@gmail.com>
2024-07-09 14:16:50 +0530

    Post Host Changes

--- lms-frontend/app/api/search/route.ts
+++ lms-frontend/app/api/search/route.ts
@@ -1,23 +1,22 @@
+import connectToDB from "@/app/server/dbconfig/dbconfig";
 import courseModel from "@/app/server/models/courseModel";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
+
+connectToDB();
 
 export async function GET(
     request: NextRequest, res: NextResponse
 ) {
     try {
         const q = request.nextUrl.searchParams.get('q');
-        console.log(q);
-
 
         const courses = await courseModel.find({
             'title': { $regex: q, $options: 'i' },
         })
-        // console.log(courses);
-        
 
         return NextResponse.json({
-            message: 'i',
+            message: 'Courses',
             courses
         }, { status: 200 })
     } catch (error: any) {

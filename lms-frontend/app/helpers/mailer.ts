2400dd1e94bac5c73220d29099b2fd3b587c0163
harsh1924 <harshrpanwar@gmail.com>
2024-07-10 10:36:14 +0530

    Some Changes

--- lms-frontend/app/helpers/mailer.ts
+++ lms-frontend/app/helpers/mailer.ts
@@ -1,21 +1,21 @@
 import nodemailer from 'nodemailer';
 
 const sendEmail = async function (email: string, subject: string, message: string) {
-    var transport = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: 587,
-        auth: {
-          user: "api",
-          pass: process.env.SMTP_PASSWORD
-        }
-      });
+  var transport = nodemailer.createTransport({
+    host: "live.smtp.mailtrap.io",
+    port: 587,
+    auth: {
+      user: "api",
+      pass: "accd926f99d2af31d757b1842a732ab8"
+    }
+  });
 
-    await transport.sendMail({
-        from: process.env.EMAIL_FROM,
-        to: email,
-        subject: subject,
-        html: message
-    })
+  await transport.sendMail({
+    from: process.env.EMAIL_FROM,
+    to: email,
+    subject: subject,
+    html: message
+  })
 }
 
 export default sendEmail;

357318c24ad65175ed85622d4d91eca33969b0a1
harsh1924 <harshrpanwar@gmail.com>
2024-07-11 10:43:27 +0530

    Some Changes

--- lms-frontend/app/(dashboard)/course/[courseId]/survey/[userId]/page.tsx
+++ lms-frontend/app/(dashboard)/course/[courseId]/survey/[userId]/page.tsx
@@ -65,10 +65,10 @@ const SurveyPage = ({
 
                         {isCourseFree ? (
                             <div>
-                                <input type="hidden" name="redirect" value={`${process.env.HOME_URL}/course/${params.courseId}/purchase`} />
+                                <input type="hidden" name="redirect" value={`https://skilltechindia.net/course/${params.courseId}/purchase`} />
                             </div>
                         ) : (
-                            <input type="hidden" name="redirect" value={`${process.env.HOME_URL}/courses/${params.courseId}/certificate`} />
+                            <input type="hidden" name="redirect" value={`https://skilltechindia.net/courses/${params.courseId}/certificate`} />
                         )}
                         <p className="py-2 source-sans-3-regular text-xl">
                             Rate the website based on the following aspects:

0fa4c85895ca55d526949eda6e8612bf25b29718
harsh1924 <harshrpanwar@gmail.com>
2024-07-15 11:23:33 +0530

    some changes

--- lms-frontend/next.config.mjs
+++ lms-frontend/next.config.mjs
@@ -5,7 +5,6 @@ const nextConfig = {
             "utfs.io"
         ]
     },
-    basePath: process.env.NODE_ENV === 'production' ? 'https://skilltechindia.net' : '',
 };
 
 export default nextConfig;

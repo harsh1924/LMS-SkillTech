acbf5292fc51cddb6c140b9678bb1fa993cec322
harsh1924 <harshrpanwar@gmail.com>
2024-07-15 14:27:46 +0530

    Nodemailer Changes

--- lms-frontend/app/helpers/mailer.ts
+++ lms-frontend/app/helpers/mailer.ts
@@ -2,19 +2,19 @@ import nodemailer from 'nodemailer';
 
 const sendEmail = async function (email: string, subject: string, message: string) {
   var transport = nodemailer.createTransport({
-    host: "live.smtp.mailtrap.io",
+    host: "sandbox.smtp.mailtrap.io",
     port: 587,
     secure: false,
     auth: {
-      user: "api",
-      pass: "accd926f99d2af31d757b1842a732ab8"
+      user: "92bca9cd86422f",
+      pass: "6588e7836d396d"
     }
   });
   console.log('hello mailer');
   console.log(email);
   
 
-  transport.sendMail({
+  await transport.sendMail({
     from: "skilltechindia.net",
     to: email,
     subject: 'subject',

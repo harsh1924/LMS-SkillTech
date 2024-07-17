177598335867612b479198fcce8d3c5131656cd4
harsh1924 <harshrpanwar@gmail.com>
2024-07-17 14:50:08 +0530

    OG Images

--- lms-frontend/app/api/og/route.tsx
+++ lms-frontend/app/api/og/route.tsx
@@ -11,9 +11,9 @@ export async function GET(req: NextRequest) {
         // console.log(searchParams);
 
         // const hasTitle = searchParams.has('title');
-        const title = "SKILLTECHINDIA.NET"
-        const heading = "Online Courses - Learn to Code Anywhere, Anytime | SkillTech"
-        const description = "Learn from best tutors of India"
+        // const title = "SKILLTECHINDIA.NET"
+        // const heading = "Online Courses - Learn to Code Anywhere, Anytime | SkillTech"
+        // const description = "Learn from best tutors of India"
 
         const imageData = await fetch(
             new URL('./ai.jpg', import.meta.url)).then((res) => res.arrayBuffer())
@@ -22,7 +22,7 @@ export async function GET(req: NextRequest) {
         return new (ImageResponse as any)(
             <div tw="flex flex-col w-full h-full bg-white">
                 <img src={imageData} tw="w-full h-[75%]" />
-                <div tw="flex flex-col px-6 pt-3">
+                {/* <div tw="flex flex-col px-6 pt-3">
                     <span tw="font-light text-2xl">
                         {title}
                     </span>
@@ -32,7 +32,7 @@ export async function GET(req: NextRequest) {
                     <span tw="font-light text-3xl pt-1">
                         {description}
                     </span>
-                </div>
+                </div> */}
             </div>
         );
     } catch (error: any) {
